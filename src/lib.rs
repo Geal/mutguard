b5836d0ad047f0218286ecc5d35bf90f3e1ed2d7
Geoffroy Couprie <geo.couprie@gmail.com>
2018-11-10 18:09:02 +0100

    add the serialization example

--- src/lib.rs
+++ src/lib.rs
@@ -80,6 +80,50 @@
 //!
 //! The guard function could be used to store the element to a file after every change
 //!
+//! ```rust
+//! #[macro_use]
+//! extern crate serde_derive;
+//! extern crate mut_guard;
+//! extern crate serde;
+//! extern crate serde_json;
+//!
+//! use mut_guard::*;
+//! use std::fs::File;
+//!
+//! #[derive(Serialize, Debug)]
+//! struct Data {
+//!     pub a: u32,
+//!     pub s: String,
+//!     pub v: Vec<u32>,
+//! }
+//!
+//! impl Guard for Data {
+//!     fn finish(&mut self) {
+//!         File::create("data.json")
+//!             .map_err(|e| {
+//!                 println!("could not create data file");
+//!             })
+//!             .and_then(|mut f| {
+//!                 serde_json::to_writer(f, self).map_err(|e| {
+//!                     println!("could not serialize data: {:?}", e);
+//!                 })
+//!             });
+//!     }
+//! }
+//!
+//! fn main() {
+//!     let mut data = MutGuard::new(Data {
+//!         a: 0,
+//!         s: "hello".to_string(),
+//!         v: vec![1, 2],
+//!     });
+//!
+//!     data.guard().s = "Hello world".to_string();
+//!     // data.json was created and now contains:
+//!     // {"a":0,"s":"Hello world","v":[1,2]}
+//! }
+//! ```
+//!
 use std::ops::{Deref, DerefMut, Drop};
 
 /// stores an inner element that must implement the `Guard` trait,
