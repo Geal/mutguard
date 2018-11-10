85d6d2ca5c3afca525569a0f0fd671b6787b1c5f
Geoffroy Couprie <geo.couprie@gmail.com>
2018-11-10 17:47:04 +0100

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -55,7 +55,7 @@
 //! ```rust
 //! # extern crate mut_guard;
 //! # use mut_guard::*;
-//! # 
+//! #
 //! # fn main() {
 //!   let v = Vec::new();
 //!
@@ -80,12 +80,12 @@
 //!
 //! The guard function could be used to store the element to a file after every change
 //!
-use std::ops::{Deref,DerefMut,Drop};
+use std::ops::{Deref, DerefMut, Drop};
 
 /// stores an inner element that must implement the `Guard` trait,
 /// and forbids mutable borrows except going through its `guard()` method.
 pub struct MutGuard<T> {
-  inner: T,
+    inner: T,
 }
 
 impl<T> Deref for MutGuard<T> {
@@ -99,200 +99,213 @@ impl<T> Deref for MutGuard<T> {
 /// Specifies a method that will be called after every time an element
 /// protected by a `Mutguard` will be mutably borrowed
 pub trait Guard {
-  fn finish(&mut self);
+    fn finish(&mut self);
 }
 
 impl<T: Guard> MutGuard<T> {
-  pub fn new(inner: T) -> MutGuard<T> {
-    MutGuard { inner }
-  }
+    pub fn new(inner: T) -> MutGuard<T> {
+        MutGuard { inner }
+    }
 
-  /// call this method to get mutable access to the underlying element
-  pub fn guard<'a>(&'a mut self) -> MutGuardBorrow<'a, T> {
-    MutGuardBorrow { inner: self }
-  }
+    /// call this method to get mutable access to the underlying element
+    pub fn guard<'a>(&'a mut self) -> MutGuardBorrow<'a, T> {
+        MutGuardBorrow { inner: self }
+    }
 
-  /// returns the wrapped element, consuming the MutGuard
-  pub fn into_inner(self) -> T {
-    self.inner
-  }
+    /// returns the wrapped element, consuming the MutGuard
+    pub fn into_inner(self) -> T {
+        self.inner
+    }
 }
 
 impl<'a, T> MutGuard<MutGuardWrapper<'a, T>> {
-  /// This method automatically generates a `Guard` implementation that will
-  /// call `f` after every time the inner element is mutably borrowed
-  pub fn wrap<F>(inner: T, f: F) -> MutGuard<MutGuardWrapper<'a, T>>
-    where F: 'a + for<'r> FnMut(&'r mut T) {
-    let wrapper = MutGuardWrapper {
-      inner, f: Box::new(f)
-    };
-    MutGuard::new(wrapper)
-  }
+    /// This method automatically generates a `Guard` implementation that will
+    /// call `f` after every time the inner element is mutably borrowed
+    pub fn wrap<F>(inner: T, f: F) -> MutGuard<MutGuardWrapper<'a, T>>
+    where
+        F: 'a + for<'r> FnMut(&'r mut T),
+    {
+        let wrapper = MutGuardWrapper {
+            inner,
+            f: Box::new(f),
+        };
+        MutGuard::new(wrapper)
+    }
 }
 
 /// Structure returned by the `Mutguard::guard()`. when this is dropped, iter
 /// will call the `Guard::finish()` method of the wrapped element
 pub struct MutGuardBorrow<'a, T: Guard> {
-  inner: &'a mut MutGuard<T>,
+    inner: &'a mut MutGuard<T>,
 }
 
-impl<'a, T: Guard> Deref for MutGuardBorrow<'a,T> {
-  type Target = T;
+impl<'a, T: Guard> Deref for MutGuardBorrow<'a, T> {
+    type Target = T;
 
-  fn deref(&self) -> &T {
-      &self.inner.inner
-  }
+    fn deref(&self) -> &T {
+        &self.inner.inner
+    }
 }
 
-impl<'a,T: Guard> DerefMut for MutGuardBorrow<'a,T> {
-  fn deref_mut(&mut self) -> &mut T {
-      &mut self.inner.inner
-  }
+impl<'a, T: Guard> DerefMut for MutGuardBorrow<'a, T> {
+    fn deref_mut(&mut self) -> &mut T {
+        &mut self.inner.inner
+    }
 }
 
-impl<'a,T: Guard> Drop for MutGuardBorrow<'a,T> {
-  fn drop(&mut self) {
-    self.inner.inner.finish();
-  }
+impl<'a, T: Guard> Drop for MutGuardBorrow<'a, T> {
+    fn drop(&mut self) {
+        self.inner.inner.finish();
+    }
 }
 
 /// `Guard` implementation returned by `MutGuard::wrap()`
 pub struct MutGuardWrapper<'a, T> {
-  inner: T,
-  f: Box<'a+FnMut(&mut T)>,
+    inner: T,
+    f: Box<'a + FnMut(&mut T)>,
 }
 
 impl<'a, T: 'a> MutGuardWrapper<'a, T> {
-  pub fn new<F>(inner: T, f: F) -> MutGuardWrapper<'a, T>
-    where F: 'a + for<'r> FnMut(&'r mut T) {
-    MutGuardWrapper { inner, f: Box::new(f) }
-  }
+    pub fn new<F>(inner: T, f: F) -> MutGuardWrapper<'a, T>
+    where
+        F: 'a + for<'r> FnMut(&'r mut T),
+    {
+        MutGuardWrapper {
+            inner,
+            f: Box::new(f),
+        }
+    }
 }
 
 impl<'a, T> Guard for MutGuardWrapper<'a, T> {
-  fn finish(&mut self) {
-    (self.f)(&mut self.inner);
-  }
+    fn finish(&mut self) {
+        (self.f)(&mut self.inner);
+    }
 }
 
-impl<'a, T> Deref for MutGuardWrapper<'a,T> {
-  type Target = T;
+impl<'a, T> Deref for MutGuardWrapper<'a, T> {
+    type Target = T;
 
-  fn deref(&self) -> &T {
-      &self.inner
-  }
+    fn deref(&self) -> &T {
+        &self.inner
+    }
 }
 
-impl<'a,T> DerefMut for MutGuardWrapper<'a,T> {
-  fn deref_mut(&mut self) -> &mut T {
-      &mut self.inner
-  }
+impl<'a, T> DerefMut for MutGuardWrapper<'a, T> {
+    fn deref_mut(&mut self) -> &mut T {
+        &mut self.inner
+    }
 }
 
 #[cfg(test)]
 mod tests {
-  use super::*;
+    use super::*;
 
-  #[derive(Debug)]
-  struct Bank {
-    accounts: Vec<i32>,
-  }
-
-  impl Bank {
-    pub fn new(accounts: Vec<i32>) -> Bank {
-      Bank { accounts }
-    }
-
-    pub fn transfer(&mut self, account1: usize, account2: usize, amount: i32) {
-      self.accounts[account1] -= amount;
-      self.accounts[account2] += amount;
-    }
-  }
-
-  impl Guard for Bank {
-    fn finish(&mut self) {
-      assert!(self.accounts.iter().any(|v| *v < 0), "accounts should not become negative");
-    }
-  }
-
-  #[test]
-  #[should_panic(expected = "accounts should not become negative")]
-  fn invariant_bank() {
-    let mut ibank = MutGuard::new(
-      Bank::new(vec!(10, 0, 20, 50))
-    );
-
-    println!("bank: {:?}", *ibank);
-
-    {
-    ibank.guard().transfer(0, 1, 5);
-    println!("bank: {:?}", *ibank);
-
-    ibank.guard().transfer(2, 3, 30);
-    println!("bank: {:?}", *ibank);
-    }
-  }
-
-  #[test]
-  fn bank() {
-    let mut bank = Bank::new(vec!(10, 0, 20, 50));
-    println!("bank: {:?}", bank);
-
-    bank.transfer(0, 1, 5);
-    println!("bank: {:?}", bank);
-
-    bank.transfer(2, 3, 30);
-    // now accounts are [5, 5, -10, 80]
-    println!("bank: {:?}", bank);
-  }
-
-  #[test]
-  fn count_access() {
-    let mut counter = 0;
-    let v = Vec::new();
-
-    {
-      let mut iv = MutGuard::wrap(v, |_| counter += 1);
-
-      iv.guard().push(1);
-      iv.guard().push(2);
-      iv.guard().push(3);
-      assert_eq!(iv[0], 1);
-      assert_eq!(iv[1], 2);
-      assert_eq!(iv[2], 3);
-    }
-
-    assert_eq!(counter, 3);
-  }
-
-  #[test]
-  #[should_panic]
-  fn less_than() {
     #[derive(Debug)]
-    struct LessThan20(pub u8);
-
-    impl Guard for LessThan20 {
-      fn finish(&mut self) {
-        assert!(self.0 <= 20, "invariant failed, internal value is too large: {}", self.0);
-      }
+    struct Bank {
+        accounts: Vec<i32>,
     }
 
-    let mut val = MutGuard::new(LessThan20(0));
+    impl Bank {
+        pub fn new(accounts: Vec<i32>) -> Bank {
+            Bank { accounts }
+        }
 
-    //"val: 0"
-    println!("val: {:?}", *val);
+        pub fn transfer(&mut self, account1: usize, account2: usize, amount: i32) {
+            self.accounts[account1] -= amount;
+            self.accounts[account2] += amount;
+        }
+    }
 
-    // this would fail because MutGuard does not implement DerefMut directly
-    //val.0 = 10;
+    impl Guard for Bank {
+        fn finish(&mut self) {
+            assert!(
+                self.accounts.iter().any(|v| *v < 0),
+                "accounts should not become negative"
+            );
+        }
+    }
 
-    // use the guard() method to get a `&mut LessThan20`
-    val.guard().0 = 10;
+    #[test]
+    #[should_panic(expected = "accounts should not become negative")]
+    fn invariant_bank() {
+        let mut ibank = MutGuard::new(Bank::new(vec![10, 0, 20, 50]));
 
-    //"val: 10"
-    println!("val: {:?}", *val);
+        println!("bank: {:?}", *ibank);
 
-    // once the value returned by guard() is dropped, the invariant will be checked
-    // we get the message "panicked at 'invariant failed, internal value is too large: 30'"
-    val.guard().0 = 30;
-  }
+        {
+            ibank.guard().transfer(0, 1, 5);
+            println!("bank: {:?}", *ibank);
+
+            ibank.guard().transfer(2, 3, 30);
+            println!("bank: {:?}", *ibank);
+        }
+    }
+
+    #[test]
+    fn bank() {
+        let mut bank = Bank::new(vec![10, 0, 20, 50]);
+        println!("bank: {:?}", bank);
+
+        bank.transfer(0, 1, 5);
+        println!("bank: {:?}", bank);
+
+        bank.transfer(2, 3, 30);
+        // now accounts are [5, 5, -10, 80]
+        println!("bank: {:?}", bank);
+    }
+
+    #[test]
+    fn count_access() {
+        let mut counter = 0;
+        let v = Vec::new();
+
+        {
+            let mut iv = MutGuard::wrap(v, |_| counter += 1);
+
+            iv.guard().push(1);
+            iv.guard().push(2);
+            iv.guard().push(3);
+            assert_eq!(iv[0], 1);
+            assert_eq!(iv[1], 2);
+            assert_eq!(iv[2], 3);
+        }
+
+        assert_eq!(counter, 3);
+    }
+
+    #[test]
+    #[should_panic]
+    fn less_than() {
+        #[derive(Debug)]
+        struct LessThan20(pub u8);
+
+        impl Guard for LessThan20 {
+            fn finish(&mut self) {
+                assert!(
+                    self.0 <= 20,
+                    "invariant failed, internal value is too large: {}",
+                    self.0
+                );
+            }
+        }
+
+        let mut val = MutGuard::new(LessThan20(0));
+
+        //"val: 0"
+        println!("val: {:?}", *val);
+
+        // this would fail because MutGuard does not implement DerefMut directly
+        //val.0 = 10;
+
+        // use the guard() method to get a `&mut LessThan20`
+        val.guard().0 = 10;
+
+        //"val: 10"
+        println!("val: {:?}", *val);
+
+        // once the value returned by guard() is dropped, the invariant will be checked
+        // we get the message "panicked at 'invariant failed, internal value is too large: 30'"
+        val.guard().0 = 30;
+    }
 }
